5d0555569c77a4d6d72f5911fdaacbaa69fb6e93
jeanette adelson <jeni.jeanette@gmail.com>
2016-07-22 17:02:54 -0500

    new game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,19 @@
 console.log("JS file is connected to HTML! Woo!")
+
+var cardOne = "Queen";
+var cardTwo = "Queen";
+var cardThree = "King";
+var carFour = "King";
+
+cardOne === cardTwo;
+'cardOne' !== cardThree;
+'cardOne' !== cardFour;
+'cardTwo' !== cardThree;
+'cardTwo' !== cardFour;
+cardThree === cardFour;
+
+if (cardOne === cardTwo) || (cardThree === cardFour) {
+	alert = "You found a match!"
+} else {
+	alert = "Sorry, try agian."
+}
