11dfe7bfdea9c2a51337046cc1a10825240a4e70
jeanette adelson <jeni.jeanette@gmail.com>
2016-07-24 21:50:14 -0500

    Unit11

--- starter-code/main.js
+++ starter-code/main.js
@@ -12,8 +12,26 @@ cardOne === cardTwo;
 'cardTwo' !== cardFour;
 cardThree === cardFour;
 
-if (cardOne === cardTwo) || (cardThree === cardFour) {
+/*if (cardOne === cardTwo) || (cardThree === cardFour) {
 	alert = "You found a match!"
 } else {
 	alert = "Sorry, try agian."
+}*/
+
+var gameBoard = document.getElementById('game-board')
+for (var i = 0; i < gameBoard.length; i++) {
+   gameBoard.innerHTML += '<div class="card"></div>';
 }
+
+var gameBoard= element.appendChild(.board);
+
+var createBoard = function();
+for (var i = 0; i< cards.length; i++);
+
+cardElement.setAttribute('data-card', cards[i]);
+
+var cards = ["queen","queen","king,","king"];
+var cardsInPlay = [];
+
+cardElement.addEventListener('click', isTwoCards)
+
